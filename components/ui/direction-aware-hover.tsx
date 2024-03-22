5c6f2842cc87d49c61301a05fdbd367a24845835
Alessandro Branchetti <brk.alessandro@gmail.com>
2024-03-22 15:17:46 +0100

    fix linter issue

--- components/ui/direction-aware-hover.tsx
+++ components/ui/direction-aware-hover.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import React, { useRef, useState } from "react";
+import React, {useRef, useState} from "react";
 import Image from "next/image";
-import { AnimatePresence, motion } from "framer-motion";
+import {AnimatePresence, motion} from "framer-motion";
 import {cn} from "@/lib/utils";
 
 export const DirectionAwareHover = ({
@@ -57,8 +57,7 @@ export const DirectionAwareHover = ({
     const { width: w, height: h, left, top } = obj.getBoundingClientRect();
     const x = ev.clientX - left - (w / 2) * (w > h ? h / w : 1);
     const y = ev.clientY - top - (h / 2) * (h > w ? w / h : 1);
-    const d = Math.round(Math.atan2(y, x) / 1.57079633 + 5) % 4;
-    return d;
+    return Math.round(Math.atan2(y, x) / 1.57079633 + 5) % 4;
   };
   
   return (
