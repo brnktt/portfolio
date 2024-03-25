75c3e63e09d9c9d527196063c396f29ca9de59db
Alessandro Branchetti <brk.alessandro@gmail.com>
2024-03-25 12:27:02 +0100

    change projects links and photos

--- app/(home)/components/Projects.tsx
+++ app/(home)/components/Projects.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import Title from "@/app/(home)/components/Title";
-import {SiNextdotjs, SiReact, SiTailwindcss} from "react-icons/si";
+import {
+  SiAmazonaws,
+  SiBootstrap,
+  SiDotnet,
+  SiMicrosoftsqlserver,
+  SiReact,
+  SiTailwindcss
+} from "react-icons/si";
 import Link from "next/link";
 import {cn} from "@/lib/utils";
 import {DirectionAwareHover} from "@/components/ui/direction-aware-hover";
@@ -8,31 +15,31 @@ import {DirectionAwareHover} from "@/components/ui/direction-aware-hover";
 function Projects() {
   const projects = [
     {
-      title: "Project 1",
-      tech: [SiNextdotjs, SiReact, SiTailwindcss],
-      link: "http://localhost:3000/",
-      cover: "/project-1.png",
+      title: "Siemens Financial Services (SFS)",
+      tech: [SiDotnet, SiMicrosoftsqlserver],
+      link: "https://www.siemens.com/global/en/company/about/businesses/financial-services.html",
+      cover: "/sfs.png",
       background: "bg-blue-500",
     },
     {
-      title: "Project 2",
-      tech: [SiNextdotjs, SiReact, SiTailwindcss],
-      link: "http://localhost:3000/",
-      cover: "/project-2.png",
+      title: "Groups360",
+      tech: [SiDotnet, SiReact, SiTailwindcss, SiAmazonaws],
+      link: "https://groups360.com/",
+      cover: "/groups360.png",
       background: "bg-green-500",
     },
     {
-      title: "Project 3",
-      tech: [SiNextdotjs, SiReact, SiTailwindcss],
-      link: "http://localhost:3000/",
-      cover: "/project-3.png",
+      title: "Mooney",
+      tech: [SiReact, SiDotnet, SiBootstrap],
+      link: "https://www.mooney.it/",
+      cover: "/mooney.png",
       background: "bg-indigo-500",
     },
     {
-      title: "Project 4",
-      tech: [SiNextdotjs, SiReact, SiTailwindcss],
-      link: "http://localhost:3000/",
-      cover: "/project-4.png",
+      title: "B-One Software",
+      tech: [SiMicrosoftsqlserver, SiDotnet, SiReact],
+      link: "https://www.b-onesoftware.com/",
+      cover: "/b-one.png",
       background: "bg-cyan-500",
     },
   ];
@@ -42,7 +49,7 @@ function Projects() {
       <Title text="Projects 💡" className="flex flex-col items-center justify-center rotate-6" />
       <div className="grid grid-cols-1 sm:grid-cols-2 pt-20 gap-5">
         {projects.map((project, index) => {
-          return <Link href={project.link} key={index}>
+          return <Link href={project.link} key={index} target="_blank">
             <div className={cn("p-5 rounded-md", project.background)}>
               <DirectionAwareHover imageUrl={project.cover} className="w-full space-y-5 cursor-pointer ">
                 <div className="space-y-5">
